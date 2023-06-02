7aa1ceb32f4b4af26f1b0bd454d982116e9753c4
Fabio Frasson <fabio.frass@gmail.com>
2023-06-02 10:31:46 -0300

    Tentando importar o svg

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -3,7 +3,9 @@ import styles from "./Menu.module.scss";
 export default function Menu() {
     return (
         <main>
-            <nav className="menu"></nav>
+            <nav className={styles.menu}>
+                <img src="../../assets/logo.svg" alt="Logo Aluroni" />
+            </nav>
         </main>
     );
 }
