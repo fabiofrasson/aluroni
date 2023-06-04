f7f8dad6e9a902eaa103ee6809f5a2ad93309ffb
Fabio Frasson <fabio.frass@gmail.com>
2023-06-04 16:08:39 -0300

    Ordenador

--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -22,12 +22,25 @@ export default function Items({ search, filter, sorter }: Props) {
         return true;
     }
 
+    function sort(list: typeof menu) {
+        switch (sorter) {
+            case "porcao":
+                return list.sort((a, b) => (a.size > b.size ? 1 : -1));
+            case "qtd_pessoas":
+                return list.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+            case "preco":
+                return list.sort((a, b) => (a.price > b.price ? 1 : -1));
+            default:
+                return list;
+        }
+    }
+
     useEffect(() => {
         const newList = menu.filter(
             (item) => searchTest(item.title) && filterTest(item.category.id)
         );
-        setList(newList);
-    }, [search, filter]);
+        setList(sort(newList));
+    }, [search, filter, sorter]);
 
     return (
         <div className={styles.items}>
