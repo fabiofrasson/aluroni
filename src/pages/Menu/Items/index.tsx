0716b79df6f16fe4aa359277dc03a2b1ea5f2a01
Fabio Frasson <fabio.frass@gmail.com>
2023-06-04 15:57:52 -0300

    Buscador e filtros

--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -1,11 +1,37 @@
 import menu from "./items.json";
 import Item from "./Item";
 import styles from "./Items.module.scss";
+import { useEffect, useState } from "react";
 
-export default function Items() {
+interface Props {
+    search: string;
+    filter: number | null;
+    sorter: string;
+}
+
+export default function Items({ search, filter, sorter }: Props) {
+    const [list, setList] = useState(menu);
+
+    function searchTest(title: string) {
+        const regex = new RegExp(search, "i");
+        return regex.test(title);
+    }
+
+    function filterTest(id: number) {
+        if (filter !== null) return filter === id;
+        return true;
+    }
+
+    useEffect(() => {
+        const newList = menu.filter(
+            (item) => searchTest(item.title) && filterTest(item.category.id)
+        );
+        setList(newList);
+    }, [search, filter]);
+
     return (
         <div className={styles.items}>
-            {menu.map((item) => (
+            {list.map((item) => (
                 <Item key={item.id} {...item} />
             ))}
         </div>
