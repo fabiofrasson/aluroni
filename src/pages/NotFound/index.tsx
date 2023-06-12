49e7213163d5f893247b71d3579ab03fedce584c
Fabio Frasson <fabio.frass@gmail.com>
2023-06-12 14:37:58 -0300

    Voltando para outra tela

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,13 +2,20 @@ import styles from './NotFound.module.scss';
 import { ReactComponent as NotFoundImage} from 'assets/not_found.svg';
 import classNames from 'classnames';
 import themeStyles from 'styles/Theme.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+  const navigate = useNavigate();
   return(
     <div className={classNames({
       [styles.container]: true,
       [themeStyles.container]: true
     })}>
+      <div className={styles.returnBtn}>
+        <button onClick={() => navigate(-1)}>
+          {'< Voltar'} 
+        </button>
+      </div>
       <NotFoundImage />
     </div>
   );
