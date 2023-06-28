fa8a46369726cbf75bea030cbf48ea17ca4036db
Fabio Frasson <fabio.frass@gmail.com>
2023-06-28 11:37:48 -0300

    useLocation e state

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,17 @@ import menu from 'data/menu.json';
 import styles from './Home.module.scss';
 import themeStyles from 'styles/Theme.module.scss';
 import restaurant from 'assets/restaurant.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
   let recommendedDishes = [...menu];
   recommendedDishes = recommendedDishes.sort(() => 0.5 -  Math.random()).splice(0,3);
+  const navigate = useNavigate();
+
+  function redirectToDetails(dish: typeof menu[0]) {
+    navigate(`/prato/${dish.id}`, {state: { ...dish }, replace: true});
+  }
+
   return (
     <section>
       <h3 className={themeStyles.title}>Recomendações da cozinha</h3>
@@ -15,7 +22,10 @@ export default function Home() {
             <div className={styles.recommendedS__image}>
               <img src={item.photo} alt={item.title} />
             </div>
-            <button className={styles.recommendedS__button}>
+            <button 
+              className={styles.recommendedS__button}
+              onClick={() => redirectToDetails(item)}
+            >
               Ver mais
             </button>
           </div>
