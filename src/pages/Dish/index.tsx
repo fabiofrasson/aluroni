f75f61a133ec153124f39a07f0e416a037b587c5
Fabio Frasson <fabio.frass@gmail.com>
2023-07-06 17:02:09 -0300

    Resolvendo o Header

--- src/pages/Dish/index.tsx
+++ src/pages/Dish/index.tsx
@@ -1,8 +1,9 @@
 import styles from './Dish.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import menu from 'data/menu.json';
 import DishTags from 'components/DishTags';
 import NotFound from 'pages/NotFound';
+import DefaultPage from 'components/DefaultPage';
 
 export default function Dish() {
   const {id} = useParams();
@@ -10,24 +11,30 @@ export default function Dish() {
   const dish = menu.find(item => item.id === Number(id));
   if(!dish) return <NotFound />;
   return (
-    <>
-      <button className={styles.return} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.title}>
-          {dish.title}
-        </h1>
-        <div className={styles.image}>
-          <img src={dish.photo} alt={dish.title} />
-        </div>
-        <div className={styles.content}>
-          <p className={styles.content__description}>
-            {dish.description}
-          </p>
-          <DishTags {...dish} />
-        </div>
-      </section>
-    </>
+    <Routes>
+      <Route path="*" element={<DefaultPage />}>
+        <Route index element={
+          <> 
+            <button className={styles.return} onClick={() => navigate(-1)}>
+              {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+              <h1 className={styles.title}>
+                {dish.title}
+              </h1>
+              <div className={styles.image}>
+                <img src={dish.photo} alt={dish.title} />
+              </div>
+              <div className={styles.content}>
+                <p className={styles.content__description}>
+                  {dish.description}
+                </p>
+                <DishTags {...dish} />
+              </div>
+            </section>
+          </>
+        }/>
+      </Route>
+    </Routes>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -18,8 +18,8 @@ export default function AppRouter() {
             <Route index element={<Home />} />
             <Route path='cardapio' element={<Menu />} />
             <Route path='sobre' element={<About />} />
-            <Route path='prato/:id' element={<Dish />} />
           </Route>
+          <Route path='prato/:id' element={<Dish />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
